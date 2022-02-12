18713f20eeb5f67fd6e668c7d24f86322f31482e
Yanna Pecanha <yannarp@hotmail.com>
2022-02-12 16:42:36 -0300

    Initial commit

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,16 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+ 
+  export interface DefaultTheme {
+    colors: {
+        primary: string,
+        secondary: string,
+        tertiary: string,
+        background: string,
+        backgroundLight: string,
+        red: string,
+        green: string
+    };
+  }
